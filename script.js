9c985e47210b873a95f100d588e24166d3364611
Jia Hao Choo <71251813+jiah29@users.noreply.github.com>
2024-02-08 00:29:45 -0500

    finalise geojson layer

--- script.js
+++ script.js
@@ -18,18 +18,26 @@ map.on("load", () => {
     data: "https://jiah29.github.io/ggr472_lab2/data/parks.geojson",
   });
 
-  map.addImage("park-alt1");
-
   // Add parks layer to map
   map.addLayer({
     id: "parks-point",
     type: "symbol", // set layer type to symbol
     source: "parks-data",
     layout: {
-      "icon-image": "park-alt1", // set icon image from mapbox style
-      "icon-size": 1.5, // set icon size
+      "icon-image": "park-alt1", // set icon image which is uploaded to mapbox studio style
+      "text-field": "{name}", // set text field to display name property in geojson
+      "text-size": 12, // set text size
+      "text-offset": [0, 1.25], // set text offset
     },
-    minzoom: 10,
-    maxzoom: 22,
+    paint: {
+      "text-color": "green", // set text color
+      "text-halo-color": "white", // set text halo color to white to make the text stand out more
+      "text-halo-width": 1, // set text halo width so that halo is visible
+      // set text opacity based on zoom level in step formats (not linear change)
+      // >= level 10 = 0.5, >= level 14 = 0.75, >= level 18 = 1
+      "text-opacity": ["step", ["zoom"], 0, 10, 0.5, 14, 0.75, 18, 1],
+    },
+    minzoom: 10, // set minimum zoom level to display the layer
+    maxzoom: 22, // set maximum zoom level to display the layer
   });
 });
